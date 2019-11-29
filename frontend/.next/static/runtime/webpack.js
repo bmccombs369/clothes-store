5387c28bd8dfe65ff3b8680eb6ac44d9b6c2d3cc
bmccombs369 <bmccombs369@gmail.com>
2019-11-29 14:34:13 -0500

    imported next router and nprogress to have progress bar

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "00727af438c4cd1a1799";
+/******/ 	var hotCurrentHash = "34ffc17c2b61c3e3f301";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 import Nav from './Nav';
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -45,7 +57,7 @@ const Header = () => (
     <div className="bar">
       <Logo>
         <Link href="/">
-          <a>Sick Fits</a>
+          <a>Dumb Supreme</a>
         </Link>
       </Logo>
       <Nav />
